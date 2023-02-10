f1de16481dfe905b8170d8f56b33de548878ecf6
Lekuba Ntoane <l.ntoane@gmail.com>
2023-02-10 22:20:00 +0200

    Create a DataFrame from a list of emails

--- share-email.py
+++ share-email.py
@@ -10,3 +10,31 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from nltk.stem.porter import PorterStemmer
 from sklearn.linear_model import LogisticRegression
 
+emails = [
+    ['I shared your email', 'shared'],
+    ['I just shared your address', 'shared'],
+    ["I've sent your email address to my friend", 'shared'],
+    ["I've shared your email", 'shared'],
+    ['I already shared email', 'shared'],
+    ["I've just shared your address", 'shared'],
+    ['Okay I have shared the email', 'shared'],
+    ['I have shared your email', 'shared'],
+    ['I did share your email', 'shared'],
+    ['I shared your contacts', 'shared'],
+    ['I shared your digits', 'shared'],
+    ['I shared your contact details', 'shared'],
+    ['I shared your contact card', 'shared'],
+    ['I shared the email with my friends', 'shared'],
+    ['I have sent this email to my friends', 'shared'],
+    ['The email has been shared with all my friends', 'shared'],
+    ['Can I share your email address', 'share'],
+    ['May I share your email', 'share'],
+    ['Might I share your email', 'share'],
+    ['Could we share your email address with my friends', 'share'],
+    ['Can I share your email with my friend', 'share'],
+    ['Can I send your email to my friend', 'share'],
+    ['Can I give your contacts with my friend?', 'share'],
+]
+
+data = pd.DataFrame(emails, columns=['email', 'label'])
+print(data)
